55c5ee68f584a3de6c86cb17549c8eb2781c8af2
JhonathanALM <jalechon@espe.edu.ec>
2018-12-13 20:13:52 -0500

    libreria

---
+++ tallerConjunta-ejb/src/main/java/ec/edu/espe/arquitectura/model/Requerimiento.java
@@ -0,0 +1,11 @@
+package ec.edu.espe.arquitectura.model;
+
+import xyz.morphia.annotations.Entity;
+
+/**
+ * @author jhona
+ */
+@Entity(noClassnameStored = true, value = "requerimiento")
+public class Requerimiento {
+    
+}
